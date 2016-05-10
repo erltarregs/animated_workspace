2d02660c66ae9dd9a0094b859b106c0e03eb9639
erltarregs <erljue@gmail.com>
2016-05-11 07:01:00 +0800

    initialize...

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Maverick's Animated Workspace</title>
+
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script src="js/toggles.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<div>
+	Hello, Maverick. Welcome to your animated workspace!
+
+	<div class="bottom-prep">
+		<div id="bottom_rule"></div>
+	</div>
+</div>
+</body>
+</html>
